23c4211315cc3889ea7a06c8cb795a11d4fc4a21
singhaditya2535-png <singhaditya2535@gmail.com>
2026-06-05 08:23:47 +0530

    Add file via upload

--- day1/program1.c
+++ day1/program1.c
@@ -5,6 +5,6 @@ int main() {
     scanf("%d,& n");
     for(i=1;i<=n;i++)
     {sum=sum+i;}
-    printf("sum of first %d natural numbers=%d",n,sum);
+    printf("sum of first %d natural numbers=%d,n,sum");
     return 0;
 }
