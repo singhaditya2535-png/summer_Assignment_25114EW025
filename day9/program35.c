49ed8a037156098f019e6af00be28fb011be1652
singhaditya2535-png <singhaditya2535@gmail.com>
2026-06-09 15:42:23 +0530

    Add files via upload

---
+++ day9/program35.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int n,i,j;
+    printf("enter the number of row: ");
+    scanf("%d",&n);
+    for(i=65;i<=69;i++){
+        for(j=65;j<=i;j++){
+            printf("%c",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
