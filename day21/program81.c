5b3a9398379b05a91ac13226eff3e9687c6964b1
singhaditya2535-png <singhaditya2535@gmail.com>
2026-06-22 19:13:32 +0530

    Add files via upload

---
+++ day21/program81.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    char str[100];
+    int i=0;
+
+    printf("Enter a string: ");
+    fgets(str,sizeof(str),stdin);
+
+    while(str[i]!='\0' && str[i]!='\n'){
+        i++;
+    }
+    printf("length of the string=%d\n",i);
+    return 0;
+}
